4f9859c9f9d258965d6005e287df07133b1f7aea
Bertalan Verebelyi <bertalan.verebelyi@gmail.com>
2016-04-02 22:05:40 +0200

    base enum

---
+++ src/BaseEnum.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace "WBoyz\LaravelEnum";
+
+use ReflectionClass;
+
+abstract class BaseEnum
+{
+    /**
+     * Returns the enum values.
+     *
+     * @return array
+     */
+    public static function getValues()
+    {
+        return array_values(static::toDictionary());
+    }
+
+    /**
+     * Returns the enum keys.
+     *
+     * @return array
+     */
+    public static function getKeys()
+    {
+        return array_keys(static::toDictionary());
+    }
+
+    /**
+     * Returns the enum as dictionary.
+     *
+     * @return array
+     */
+    public static function toDictionary()
+    {
+        $className = get_called_class();
+        $reflectionClass = new ReflectionClass($className);
+
+        return $reflectionClass->getConstants();
+    }
+}
